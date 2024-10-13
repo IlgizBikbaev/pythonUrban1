b101ea59954f48a742c8670bb475e938f6453485
IlgizBikbaev <131395377+IlgizBikbaev@users.noreply.github.com>
2024-10-13 15:05:11 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,14 @@
+my_string = input()
+# Вывести количество символов текста
+sym_my_string = len(my_string)
+print(sym_my_string)
+#Вывести строку my_string в верхнем регистре
+print(my_string.upper())
+#Вывеcти строку my_string в нижнем регистре
+print(my_string.lower())
+# Изменить строку my_string, удалив все пробелы
+print(my_string.replace(' ', ''))
+#Вывести первый символ строки my_string
+print(my_string[0])
+#Вывеcти последний символ строки my_string
+print(my_string[-1])
